10af82c9ed6d60a9daa2aa4f3974339f3672ce96
Discord Digital <56228439+DiscordDigital@users.noreply.github.com>
2020-05-04 11:35:09 +0200

    Create fuck0ff.sh
    Features: Theme patching, changing of iOS root mount point, quick fix for iOS filenames that don't work on Windows.

---
+++ fuck0ff.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+iosroot=/mnt
+
+argument=$1
+
+if [ -z "$1" ];
+then
+    echo Please specify theme to patch
+    exit
+else
+    echo Patching..
+fi
+
+for f in $iosroot/private/var/mobile/Containers/Data/Application/*; do
+    testpath=$f/Library/Caches/TelephonyUI-7
+    if [ -f "$testpath/en-0-+--mask.png" ] || [ -f "$testpath/other-0-+--mask.png" ];
+    then
+        searchfolder=$f
+    fi
+done
+
+
+if [ ! -z "$searchfolder" ];
+then
+    uipath=$searchfolder/Library/Caches/TelephonyUI-7
+
+    if [ "$1" == "remove" ];
+    then
+        rm -rf $uipath
+        echo Restart phone app to regenerate cache
+        exit
+    fi
+
+    unzip -o $1 -d $uipath
+    echo Quick fixup
+    if [ -f "$uipath/en-_---white.png" ];
+    then
+        mv $uipath/en-_---white.png $uipath/en-\*---white.png
+    fi
+    if [ -f "$uipath/en-_---mask.png" ];
+    then
+        mv $uipath/en-_---mask.png $uipath/en-\*---mask.png
+    fi
+    if [ -f "$uipath/other-_---white.png" ];
+    then
+        mv $uipath/other-_---white.png $uipath/other-\*---white.png
+    fi
+    if [ -f "$uipath/other-_---mask.png" ];
+    then
+        mv $uipath/other-_---mask.png $uipath/other-\*---mask.png
+    fi
+    echo Restart phone app to see effect
+else
+    echo Could not find phone folder
+fi
